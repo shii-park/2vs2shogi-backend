8c1dad8b6cb88d7598a49c779868765ef1116fb4
Suiren91 <rento8191@gmail.com>
2025-12-09 09:32:18 +0900

    #2 Fix: フィールド名を変更

--- src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/Piece.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/Piece.java
@@ -9,31 +9,54 @@ public class Piece {
     private final PieceType type;
     private Team team;
     private Position position;
-    private boolean promoted; //成りの有無
-    private final boolean promotable; //成れるかどうか
+    private boolean isPromoted; // 成りの有無
+    private final boolean isPromotable; // 成れるかどうか
 
-    //ownerIdについては要検討
-    public Piece(int id,PieceType type,Team team,Position position,boolean promotable){
-        this.id=id;
-        this.type=type;
-        this.team=team;
-        this.position=position;
-        this.promoted=false;
-        this.promotable=promotable;
+    // ownerIdについては要検討
+    public Piece(int id, PieceType type, Team team, Position position, boolean promotable) {
+        this.id = id;
+        this.type = type;
+        this.team = team;
+        this.position = position;
+        this.isPromoted = false;
+        this.isPromotable = promotable;
     }
 
-    public int getId(){return id;}
-    public Team getTeam(){return team;}
-    public PieceType getType(){return type;}
-    public Position getPosition(){return position;}
-    public void setPosition(Position p){this.position=p;}
-    public boolean isPromoted(){return promoted;}
-    public void setPromoted(boolean p){this.promoted=p;}
-    public boolean isPromotable(){return promotable;}
+    public int getId() {
+        return id;
+    }
 
-    //デバック用
+    public Team getTeam() {
+        return team;
+    }
+
+    public PieceType getType() {
+        return type;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position p) {
+        this.position = p;
+    }
+
+    public boolean getPromoted() {
+        return isPromoted;
+    }
+
+    public void setPromoted(boolean p) {
+        this.isPromoted = p;
+    }
+
+    public boolean getPromotable() {
+        return isPromotable;
+    }
+
+    // デバック用
     @Override
-    public String toString(){
-        return "Piece{"+id+","+type+".team="+team+",pos="+position+"}";
+    public String toString() {
+        return "Piece{" + id + "," + type + ".team=" + team + ",pos=" + position + "}";
     }
 }
