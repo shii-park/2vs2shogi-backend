ff12f01791e53ae7748ae62f457931c79663a820
Suiren91 <rento8191@gmail.com>
2025-12-09 19:20:59 +0900

    #2 Add: 駒の捕獲を管理するクラスを追加

---
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/CapturedPieces.java
@@ -0,0 +1,18 @@
+package com.github.com.shii_park.shogi2vs2.model.domain;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import com.github.com.shii_park.shogi2vs2.model.enums.Team;
+
+public class CapturedPieces {
+    Map<Team, List<Piece>> piecesByTeam;
+
+    public CapturedPieces() {
+        piecesByTeam = new ConcurrentHashMap<>();
+    }
+
+    public void Captured(Team team, Piece piece) {
+        piecesByTeam.get(team).add(piece);
+    }
+}
