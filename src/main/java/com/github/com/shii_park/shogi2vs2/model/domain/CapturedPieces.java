893eace901a00495ea32d353c7b9b4ae51031191
Suiren91 <rento8191@gmail.com>
2025-12-10 16:19:03 +0900

    #2 Add: 捕獲時に成りをもとに戻す処理を追加

--- src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/CapturedPieces.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/CapturedPieces.java
@@ -26,6 +26,7 @@ public class CapturedPieces {
             // ゲーム終了
         }
         capturedPieces.get(team).add(piece);
+        piece.setPromoted(false);
         piece.setTeam(team);
     }
 }
