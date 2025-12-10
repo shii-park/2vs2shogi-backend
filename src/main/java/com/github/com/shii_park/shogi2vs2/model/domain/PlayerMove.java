ec6f708408a87ccfd42262be343c1977aa2c1216
Suiren91 <rento8191@gmail.com>
2025-12-10 23:57:04 +0900

    #2 Add: PLayerMoveに移動時刻を追加

--- src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/PlayerMove.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/PlayerMove.java
@@ -1,8 +1,8 @@
 package com.github.com.shii_park.shogi2vs2.model.domain;
 
+import java.time.Instant;
 import java.util.List;
 import com.github.com.shii_park.shogi2vs2.model.enums.Direction;
-import com.github.com.shii_park.shogi2vs2.model.enums.PieceType;
 
-public record PlayerMove(Player player, Piece piece, List<Direction> direction) {
+public record PlayerMove(Player player, Piece piece, List<Direction> direction, Instant at) {
 }
