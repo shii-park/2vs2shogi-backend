92a02494d2387ac02bac0c8dbafae2e5fba812a4
Suiren91 <rento8191@gmail.com>
2025-12-10 23:57:24 +0900

    #2 Add: Playerに投了フィールドを追加

--- src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/Player.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/Player.java
@@ -6,12 +6,14 @@ import com.github.com.shii_park.shogi2vs2.model.enums.Team;
 public class Player {
     private final String id;
     private final Team team;
+    private boolean resign;
     private boolean connected;
 
     public Player(String id, Team team) {
         this.id = id;
         this.team = team;
         this.connected = true;
+        this.resign = false;
     }
 
     public boolean isConnected() {
