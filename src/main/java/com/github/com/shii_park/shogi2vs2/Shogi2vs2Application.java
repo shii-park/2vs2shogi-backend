33927ecae7ad7464eb7dcbbf6b3be18866c9b59f
Naru9595 <narunaru9595@gmail.com>
2025-12-15 11:32:31 +0900

    feat: 二人の判定がそろっているか判定するロジック、ターン時間管理サービスを実装

--- src/main/java/com/github/com/shii_park/shogi2vs2/Shogi2vs2Application.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/Shogi2vs2Application.java
@@ -2,8 +2,10 @@ package com.github.com.shii_park.shogi2vs2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Shogi2vs2Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/github/com/shii_park/shogi2vs2/dto/request/MoveRequest.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/dto/request/MoveRequest.java
@@ -2,22 +2,26 @@ package com.github.com.shii_park.shogi2vs2.dto.request;
 
 import com.github.com.shii_park.shogi2vs2.model.domain.Position;
 
+import jakarta.validation.constraints.NotNull;
+
 public class MoveRequest {
-    private final String playerId;
+    @NotNull
+    private final String userId;
     private final Position from;
+    @NotNull
     private final Position to;
     private final boolean promote;
     //public final long clientTurnId; //必要であれば追加
 
-    public MoveRequest(String playerId,Position from,Position to,boolean promote){
-        this.playerId=playerId;
+    public MoveRequest(String userId,Position from,Position to,boolean promote){
+        this.userId=userId;
         this.from=from;
         this.to=to;
         this.promote=promote;
         //clientTurnId使うなら追加
     }
 
-    public String getPlayerId() {return playerId;}
+    public String getuserId() {return userId;}
     public Position getFrom() {return from;}
     public Position getTo() {return to;}
     public boolean isPromote() {return promote;}
