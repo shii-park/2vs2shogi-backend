651820a9d84aa843030eaed69390c5e33aeb01c0
Suiren91 <rento8191@gmail.com>
2025-12-17 20:18:03 +0900

    #2 Add: PlayerMoveにpromoteフィールドを追加

--- src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/PlayerMove.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/PlayerMove.java
@@ -4,5 +4,5 @@ import java.time.Instant;
 import java.util.List;
 import com.github.com.shii_park.shogi2vs2.model.enums.Direction;
 
-public record PlayerMove(Player player, Piece piece, List<Direction> direction, Instant at) {
+public record PlayerMove(Player player, Piece piece, List<Direction> direction, boolean promote, Instant at) {
 }
