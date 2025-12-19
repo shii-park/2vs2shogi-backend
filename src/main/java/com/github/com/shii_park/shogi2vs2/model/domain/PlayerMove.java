6f8e81b7084bc7b4c56b8ea2b5d6acdc6b94da8b
Suiren91 <rento8191@gmail.com>
2025-12-19 14:46:53 +0900

    #26 Fix: PlayerMoveの必要ない要素を削除

--- src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/PlayerMove.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/PlayerMove.java
@@ -1,10 +1,8 @@
 package com.github.com.shii_park.shogi2vs2.model.domain;
 
-import java.time.Instant;
 import java.util.List;
 import com.github.com.shii_park.shogi2vs2.model.enums.Direction;
 
-//TODO: resignを手に含める
 /**
  * PlayerMoveクラスは各プレイヤーの1ターンでの行動を管理し、それらのゲッターを提供します
  * 
@@ -12,9 +10,8 @@ import com.github.com.shii_park.shogi2vs2.model.enums.Direction;
  * @param piece     操作する駒
  * @param direction 移動する方向(のリスト)
  * @param promote   成るかどうか
- * @param at        行動を決定した時間
  * 
  * @author Suiren91
  */
-public record PlayerMove(Player player, Piece piece, List<Direction> direction, boolean promote, Instant at) {
+public record PlayerMove(Player player, Piece piece, List<Direction> direction, boolean promote) {
 }
