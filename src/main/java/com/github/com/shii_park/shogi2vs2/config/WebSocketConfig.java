89bbb0e9e606e2153917d90fb1ea8aecb31983c6
Naru9595 <narunaru9595@gmail.com>
2025-12-19 10:48:59 +0900

    feat: WebSocketConfigのファイルを追加

---
+++ src/main/java/com/github/com/shii_park/shogi2vs2/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.github.com.shii_park.shogi2vs2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.github.com.shii_park.shogi2vs2.handler.GameWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private GameWebSocketHandler gameWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(gameWebSocketHandler, "/ws/game")
+                .setAllowedOrigins("*");// フロントのURLを指定
+    }
+    
+}
