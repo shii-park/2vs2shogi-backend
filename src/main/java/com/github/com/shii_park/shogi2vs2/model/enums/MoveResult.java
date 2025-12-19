78247e171f3279380919c2506d4a84b533d6f770
Suiren91 <rento8191@gmail.com>
2025-12-19 15:18:14 +0900

    #27 Add: MoveResultにDROPPED(手駒から駒を配置)を追加

--- src/main/java/com/github/com/shii_park/shogi2vs2/model/enums/MoveResult.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/enums/MoveResult.java
@@ -1,12 +1,12 @@
 package com.github.com.shii_park.shogi2vs2.model.enums;
 
 /**
- * DROPPED: 駒が盤面の外に出た
+ * FALLED: 駒が盤面の外に出た
  * STACKED: 駒が自チームの駒の上に積まれた
  * CAPTURED: 駒が相手チームの駒を捕獲した
  * MOVED: 駒が移動した
+ * DROPPED: 手駒から駒を配置した
  */
 public enum MoveResult {
-    FALLED, STACKED, CAPTURED, MOVED;
+    FALLED, STACKED, CAPTURED, MOVED,  DROPPED;
 }
-//TODO: DROPPEDからFALLEDに変更
