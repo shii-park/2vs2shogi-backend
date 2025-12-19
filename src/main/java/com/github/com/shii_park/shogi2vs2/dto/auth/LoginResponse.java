2c63fb9205f26dba524fe9c530685c78ed7a1da5
Suiren91 <rento8191@gmail.com>
2025-12-19 15:11:38 +0900

    Fix: LoginResponseをレコードクラスに変更

--- src/main/java/com/github/com/shii_park/shogi2vs2/dto/auth/LoginResponse.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/dto/auth/LoginResponse.java
@@ -1,14 +1,4 @@
 package com.github.com.shii_park.shogi2vs2.dto.auth;
 
-import lombok.Data;
-@Data
-public class LoginResponse {
-    private String userId;     //ユーザーID
-    private String username;   //ユーザー名
-
-    public LoginResponse(String userId,String username){
-        this.userId = userId;
-        this.username = username;
-    }
+public record LoginResponse(String userId, String username) {
 }
-
