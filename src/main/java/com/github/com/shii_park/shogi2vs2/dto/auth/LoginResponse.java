514900fd476de7200519c28fb691ed4e4c1a65fd
Suiren91 <rento8191@gmail.com>
2025-12-21 03:08:39 +0900

    LoginResponse実装

--- src/main/java/com/github/com/shii_park/shogi2vs2/dto/auth/LoginResponse.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/dto/auth/LoginResponse.java
@@ -1,4 +1,14 @@
 package com.github.com.shii_park.shogi2vs2.dto.auth;
 
-public record LoginResponse(String userId, String username) {
+import lombok.Data;
+
+@Data
+public class LoginResponse {
+    private String userId; // ユーザーID
+    private String username; // ユーザー名
+
+    public LoginResponse(String userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
 }
