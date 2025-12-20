d3a5fdc75ee469bd3c4e7823b7dbafe4e993badf
Naru9595 <narunaru9595@gmail.com>
2025-12-21 05:33:36 +0900

    fix: TurnExecutionResultの修正

--- src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/TurnExecutionResult.java
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/TurnExecutionResult.java
@@ -2,10 +2,11 @@ package com.github.com.shii_park.shogi2vs2.model.domain;
 
 import java.util.List;
 
-public record TurnExecutionResult (
-    String type,
-    String pieceId,
-    List<String> derections,
-    String team,
-    boolean promoted
-){}
+public record TurnExecutionResult(
+    String type,        // "moveResult" or "dropResult"
+    int pieceId,        // 駒のID
+    String pieceType,   // 追加: "FU", "HI" など
+    List<String> directions, 
+    String teamId,
+    boolean promote
+) {}
