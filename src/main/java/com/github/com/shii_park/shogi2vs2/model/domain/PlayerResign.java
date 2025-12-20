de49721cd9cffd70bf8b05cda224fe8522827e66
Suiren91 <rento8191@gmail.com>
2025-12-20 13:19:15 +0900

    #28 Add: PlayerResignクラスを追加

---
+++ src/main/java/com/github/com/shii_park/shogi2vs2/model/domain/PlayerResign.java
@@ -0,0 +1,10 @@
+package com.github.com.shii_park.shogi2vs2.model.domain;
+
+/**
+ * PlayerResignクラスはプレイヤーが投了するときの操作を表現したドメインオブジェクトです
+ * 
+ * @param player     投了するプレイヤー
+ * @param turnNumber 投了時のターン数S
+ */
+public record PlayerResign(Player player, int turnNumber) {
+}
